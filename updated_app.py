55a4c632f5f1e69beef08dfaa965d89ecc560933
premkumar347 <nayudupremkumar@gmail.com>
2026-05-13 13:54:11 +0530

    Update updated_app.py

--- updated_app.py
+++ updated_app.py
@@ -1,1 +1,54 @@
-print('AI Financial Intelligence Platform Ready')
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Financial Intelligence",
+    layout="wide"
+)
+
+st.title("AI Financial Intelligence Platform")
+
+stock = st.selectbox(
+    "Select Stock",
+    ["AAPL","MSFT","GOOGL","AMZN","TSLA"]
+)
+
+signals = {
+    "AAPL":"BUY",
+    "MSFT":"BUY",
+    "GOOGL":"HOLD",
+    "AMZN":"SELL",
+    "TSLA":"SELL"
+}
+
+confidence = {
+    "AAPL":"82%",
+    "MSFT":"77%",
+    "GOOGL":"65%",
+    "AMZN":"59%",
+    "TSLA":"62%"
+}
+
+risk = {
+    "AAPL":"LOW",
+    "MSFT":"LOW",
+    "GOOGL":"MEDIUM",
+    "AMZN":"HIGH",
+    "TSLA":"HIGH"
+}
+
+st.metric(
+    "Recommendation",
+    signals[stock]
+)
+
+st.metric(
+    "Confidence",
+    confidence[stock]
+)
+
+st.metric(
+    "Risk",
+    risk[stock]
+)
+
+st.line_chart([1,2,3,4,5])
